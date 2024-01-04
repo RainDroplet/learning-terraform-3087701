668ba3b53dad3777a37d551bf5f157845e84d444
Ryan <22482214+RainDroplet@users.noreply.github.com>
2024-01-04 09:32:39 -0800

    load balancer fix

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "alb" {
   load_balancer_type = "application"
   vpc_id             = module.blog_vpc.vpc_id
   subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = {
     ex-instance = {
@@ -70,8 +70,9 @@ module "alb" {
 
   http_tcp_listeners = [
     {
-      port = 80
-      protocol
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
     }
   ]
 
